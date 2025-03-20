998f45aa3fbd7bacb27093061bbecd35d2ebf160
nearbys-online <admin@nearbys.online>
2025-03-20 10:43:54 +0400

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,23 @@
+self.addEventListener('install', (event) => {
+  console.log('Service Worker installed');
+  event.waitUntil(
+    caches.open('nearbys-cache').then((cache) => {
+      return cache.addAll([
+        '/',
+        '/index.html',
+        '/manifest.json',
+        '/service-worker.js',
+        'https://fonts.googleapis.com/css2?family=Righteous&display=swap',
+        'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css'
+      ]);
+    })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
