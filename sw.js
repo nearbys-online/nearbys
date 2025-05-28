c2848c160457fe9f6d11c90b92ac234bd507d9c5
nearbys-online <admin@nearbys.online>
2025-05-28 07:40:59 +0400

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,8 @@
+self.addEventListener('install', function(event) {
+  console.log('Service worker installed');
+  self.skipWaiting();
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(fetch(event.request));
+});
