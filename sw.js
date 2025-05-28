3008e151d8331c41cf4cbdb28b483784a01d83e6
nearbys-online <admin@nearbys.online>
2025-05-28 08:27:42 +0400

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,8 +1,22 @@
-self.addEventListener('install', function(event) {
-  console.log('Service worker installed');
-  self.skipWaiting();
+self.addEventListener('install', function (e) {
+  console.log('[ServiceWorker] Install');
+  e.waitUntil(
+    caches.open('nearby-cache').then(function (cache) {
+      return cache.addAll([
+        '/',
+        '/vendors.html',
+        '/manifest.json',
+        'https://cdn.shopify.com/s/files/1/0947/2340/8240/files/icon-192.png?v=1748402115',
+        'https://cdn.shopify.com/s/files/1/0947/2340/8240/files/icon_512.png?v=1748402101'
+      ]);
+    })
+  );
 });
 
-self.addEventListener('fetch', function(event) {
-  event.respondWith(fetch(event.request));
+self.addEventListener('fetch', function (e) {
+  e.respondWith(
+    caches.match(e.request).then(function (response) {
+      return response || fetch(e.request);
+    })
+  );
 });
