2722a50e2124507a56c220a14ec2d37c75bc5c19
nearbys-online <admin@nearbys.online>
2025-06-19 15:28:47 +0400

    Update booking-stores.js

--- booking-stores.js
+++ booking-stores.js
@@ -1,6 +1,6 @@
   // JSON map of collection titles to custom IDs
   const collectionRedirectMap = {
-    "AjGrocery": "112",
+    "ajgrocery": "112",
     "Miamix": "112",
     "Bj": "AAA888"
     // Add more mappings as needed
