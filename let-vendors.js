c52af093f9c6b3ec91244e7a4cdf796dd132de6b
nearbys-online <admin@nearbys.online>
2025-07-07 09:59:26 +0400

    Delete let-vendors.js

--- let-vendors.js
+++
@@ -1,8 +0,0 @@
-let vendors = {
-"aj": { lat: 24.385, lng: 54.584, categories: ["Grocery", "Laundry"] },
-"gh": { lat: 24.400, lng: 54.588, categories: ["Cafeteria"] },
-"jithu": { lat: 24.375, lng: 54.586, categories: ["Salon", "Grocery"] },
-"b": { lat: 24.410, lng: 54.582, categories: ["Cafeteria", "Grocery"] },    
-"c": { lat: 24.373, lng: 54.580, categories: ["Salon"] },
-"r": { lat: 24.255, lng: 54.627, categories: ["Grocery"] }
-};
